bb5b1ca08484b7d755679bd11bf83b9359ef0eb2
Ezgigunayy <gnyezgi13@gmail.com>
2022-11-20 18:30:33 +0300

    proje başlangıç

---
+++ PROJE/PROJE/Form1.cs
@@ -0,0 +1,30 @@
+namespace PROJE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
